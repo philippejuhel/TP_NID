502dc576856cf5c8bae6dbd5e1703011a9577fb1
philippe juhel <philippe.juhel@icam.fr>
2023-12-05 18:55:34 +0100

    First version

---
+++ AI/Picovoice/test_keywords_porcupine.py
@@ -0,0 +1,32 @@
+import pyaudio
+import pvporcupine
+import struct
+
+# Recognize two words (see keywords)
+
+access_key='RECOPIEZ ICI VOTRE ACCESS_KEY'
+keywords=['picovoice', 'bumblebee']
+
+#Allowed keywords : nok google, hey google, hey siri, pico clock, hey barista, computer, porcupine, terminator, grapefruit, blueberry, americano, bumblebee, grasshopper, jarvis, alexa, picovoice
+
+# Create an Porcupine instance to process the sound and recognize words
+porcupine = pvporcupine.create(
+  access_key=access_key,
+  keywords=keywords # This two keyworks will be recognized
+)
+
+# Define the audio interface to record sound
+py_audio = pyaudio.PyAudio()
+audio_stream = py_audio.open(
+    rate=porcupine.sample_rate,
+    channels=1,
+    format=pyaudio.paInt16,
+    input=True,
+    frames_per_buffer=porcupine.frame_length)
+
+while True:
+    pcm = audio_stream.read(porcupine.frame_length)  # Read an audio sample
+    audio_frame = struct.unpack_from("h" * porcupine.frame_length, pcm)
+    result = porcupine.process(audio_frame)  # and ask Porcupine to try to identify a keyword
+    if result > -1:  # A keyword has been detected
+        print(f"keyword detected : {keywords[result]}") # So we print it.
